43469f08317d388199e0f1d11ed4e762a3679f0c
Jeremy Leyvraz <jeremy.leyvraz@kep-technologies.com>
2024-02-01 08:15:19 +0100

    works

--- app/src/main/java/com/lj/app/MainActivity.kt
+++ app/src/main/java/com/lj/app/MainActivity.kt
@@ -1,23 +1,55 @@
 package com.lj.app
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.lj.app.navigation.MainNavigation
+import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.Composable
 import com.lj.app.ui.theme.TemplateTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * Main activity: define the navigation and the theme.
+ *
+ * Ex: https://www.droidcon.com/2022/10/03/adaptive-ui-with-jetpack-compose/
  */
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TemplateTheme {
-                MainNavigation()
+                val windowSizeClass = calculateWindowSizeClass(this)
+                MyApp(windowSizeClass = windowSizeClass)
             }
         }
     }
+}
+
+@Composable
+fun MyApp(windowSizeClass: WindowSizeClass) {
+    when (windowSizeClass.widthSizeClass) {
+        WindowWidthSizeClass.Compact -> {
+            CompactUI()
+        }
+        else -> {
+            ExpandedUI()
+        }
+    }
+}
+
+@Composable
+fun CompactUI() {
+    Text(text = "Compact")
+}
+
+@Composable
+fun ExpandedUI() {
+    Text(text = "Expanded")
 }
