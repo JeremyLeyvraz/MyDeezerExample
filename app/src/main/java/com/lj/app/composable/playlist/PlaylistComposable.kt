6020870e0c09179f895070491881b8fe882c1b61
Jeremy Leyvraz <jeremy.leyvraz@kep-technologies.com>
2024-02-06 08:17:48 +0100

    update ui

--- app/src/main/java/com/lj/app/composable/playlist/PlaylistComposable.kt
+++ app/src/main/java/com/lj/app/composable/playlist/PlaylistComposable.kt
@@ -1,18 +1,23 @@
 package com.lj.app.composable.playlist
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -21,6 +26,7 @@ import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -48,75 +54,32 @@ fun PlaylistComposable(windowSizeClass: WindowSizeClass, navController: NavContr
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun Compact(navController: NavController, viewModel: PlaylistViewModel) {
-    Column() {
-//        Row(modifier = Modifier.align(Alignment.CenterHorizontally)) {
-//            Surface(modifier = Modifier.fillMaxWidth()) {
-//                DisplayImage(albumId = viewModel.image, Modifier.fillMaxWidth().aspectRatio(1f))
-//            }
-//        }
-//        Row(modifier = Modifier.align(Alignment.CenterHorizontally)) {
-//            Text(
-//                text = viewModel.name,
-//                style = MaterialTheme.typography.labelLarge.copy(
-//                    fontSize = 40.sp, // Set your desired text size
-//                    fontWeight = FontWeight.Bold // Make the text bold
-//                )
-//            )
-//        }
-//        Row {
-//            Text(text = viewModel.currentMusicName.value)
-//        }
+    Column {
         Row {
             LazyColumn {
-
-
                 item {
                         Surface(modifier = Modifier.fillMaxWidth()) {
                             DisplayImage(albumId = viewModel.image, Modifier.fillMaxWidth().aspectRatio(1f))
                         }
-
                         Text(
                             text = viewModel.name,
                             style = MaterialTheme.typography.labelLarge.copy(
-                                fontSize = 40.sp, // Set your desired text size
-                                fontWeight = FontWeight.Bold // Make the text bold
+                                fontSize = 40.sp,
+                                fontWeight = FontWeight.Bold
                             )
                         )
-
-                        Text(text = viewModel.currentMusicName.value)
-
                 }
-
-                items(viewModel.getMusics()) { item ->
+                itemsIndexed(viewModel.getMusics()) { index, item ->
                     Card(onClick = {
                         //navController.navigate(MUSIC_DESTINATION+"/"+viewModel.guid+"/"+item.music)
                         viewModel.play(item.name)
                     }) {
                         MusicComposable(item, viewModel.currentMusicName.value == item.name)
                     }
-                }
-                items(viewModel.getMusics()) { item ->
-                    Card(onClick = {
-                        //navController.navigate(MUSIC_DESTINATION+"/"+viewModel.guid+"/"+item.music)
-                        viewModel.play(item.name)
-                    }) {
-                        MusicComposable(item, viewModel.currentMusicName.value == item.name)
-                    }
-                }
-                items(viewModel.getMusics()) { item ->
-                    Card(onClick = {
-                        //navController.navigate(MUSIC_DESTINATION+"/"+viewModel.guid+"/"+item.music)
-                        viewModel.play(item.name)
-                    }) {
-                        MusicComposable(item, viewModel.currentMusicName.value == item.name)
-                    }
-                }
-                items(viewModel.getMusics()) { item ->
-                    Card(onClick = {
-                        //navController.navigate(MUSIC_DESTINATION+"/"+viewModel.guid+"/"+item.music)
-                        viewModel.play(item.name)
-                    }) {
-                        MusicComposable(item, viewModel.currentMusicName.value == item.name)
+
+                    // Add a separator if the item is not the last one
+                    if (index < viewModel.getMusics().size - 1) {
+                        Divider()
                     }
                 }
             }
@@ -129,18 +92,41 @@ private fun Compact(navController: NavController, viewModel: PlaylistViewModel) 
 @Composable
 private fun Medium(navController: NavController, viewModel: PlaylistViewModel) {
     Row {
-        Column {
-            Text(text = "Titre playlist")
-            Text(text = viewModel.currentMusicName.value)
+        Surface(modifier = Modifier.fillMaxHeight()) {
+            Column {
+                // DisplayImage with aspect ratio 1:1
+                DisplayImage(albumId = viewModel.image, modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .aspectRatio(1f)
+                )
+
+                // Text component
+                Text(
+                    text = viewModel.name,
+                    style = MaterialTheme.typography.labelLarge.copy(
+                        fontSize = 40.sp,
+                        fontWeight = FontWeight.Bold
+                    ),
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(0.2f) // Adjust the weight as needed for proper distribution
+                )
+            }
         }
         Column {
             LazyColumn {
-                items(viewModel.getMusics()) { item ->
+                itemsIndexed(viewModel.getMusics()) { index, item ->
                     Card(onClick = {
                         //navController.navigate(MUSIC_DESTINATION+"/"+viewModel.guid+"/"+item.music)
                         viewModel.play(item.name)
                     }) {
                         MusicComposable(item, viewModel.currentMusicName.value == item.name)
+                    }
+
+                    // Add a separator if the item is not the last one
+                    if (index < viewModel.getMusics().size - 1) {
+                        Divider()
                     }
                 }
             }
@@ -156,17 +142,27 @@ fun MusicComposable(music: Music, isPlaying: Boolean) {
             .height(80.dp))
         Column(modifier = Modifier
             .fillMaxWidth()
+            .align(Alignment.CenterVertically)
             .weight(1.0f)) {
-            Text(text = music.name)
-            Text(text = music.artist)
+            Text(
+                text = music.name,
+                style = MaterialTheme.typography.labelLarge.copy(
+                    fontSize = 20.sp,
+                )
+            )
+            Text(
+                text = music.artist,
+                style = MaterialTheme.typography.labelLarge.copy(
+                    fontSize = 14.sp, // Set your desired text size
+                )
+            )
         }
         if(isPlaying) {
             Column(
                 modifier = Modifier
-                    .width(80.dp)
-                    .height(80.dp)
+                    .align(Alignment.CenterVertically)
             ) {
-                Text(text = "EN COURS")
+                Icon(Icons.Default.Notifications, contentDescription = null, tint = Color.Black)
             }
         }
     }
@@ -177,7 +173,16 @@ fun MusicComposable(music: Music, isPlaying: Boolean) {
 private fun Expanded(navController: NavController, viewModel: PlaylistViewModel) {
     Row {
         Column {
-            Text(text = "Titre playlist")
+            Surface(modifier = Modifier.fillMaxHeight()) {
+                DisplayImage(albumId = viewModel.image, Modifier.fillMaxWidth().aspectRatio(1f))
+            }
+            Text(
+                text = viewModel.name,
+                style = MaterialTheme.typography.labelLarge.copy(
+                    fontSize = 40.sp, // Set your desired text size
+                    fontWeight = FontWeight.Bold // Make the text bold
+                )
+            )
             Text(text = viewModel.currentMusicName.value)
         }
         Column {
