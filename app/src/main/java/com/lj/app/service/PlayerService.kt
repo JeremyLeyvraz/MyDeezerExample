dee114236613be6135331ab55608099f34b08f8b
Jeremy Gamer <jeremy.leyvraz@gmail.com>
2024-02-11 17:33:26 +0100

    icone

--- app/src/main/java/com/lj/app/service/PlayerService.kt
+++ app/src/main/java/com/lj/app/service/PlayerService.kt
@@ -7,7 +7,6 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.IBinder
 import android.support.v4.media.session.MediaSessionCompat
 import androidx.core.app.NotificationCompat
