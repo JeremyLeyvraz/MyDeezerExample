d300bb740c5e9ac66f421ad5166e1517b6d88140
Jeremy Leyvraz <jeremy.leyvraz@kep-technologies.com>
2024-02-28 08:35:23 +0100

    migration in progress

--- app/src/main/java/com/lj/app/MainActivity.kt
+++ app/src/main/java/com/lj/app/MainActivity.kt
@@ -1,37 +1,27 @@
 package com.lj.app
 
 import android.Manifest
-import android.annotation.SuppressLint
-import android.app.PendingIntent
-import android.content.ComponentName
 import android.content.Intent
-import android.content.pm.PackageManager
 import androidx.compose.material3.*
 import android.os.Bundle
-import android.widget.RemoteViews
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
-import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import androidx.media3.session.MediaController
-import androidx.media3.session.SessionToken
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberPermissionState
-import com.google.common.util.concurrent.MoreExecutors
 import com.lj.app.navigation.MainNavigation
 import com.lj.app.service.PlayerService
 import com.lj.app.viewmodel.PlayerViewModel
+import com.lj.app.viewmodel.UIEvents
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -48,7 +38,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetAudioTheme {
                 // A surface container using the 'background' color from the theme
                 val permissionState = rememberPermissionState(
                     permission = Manifest.permission.READ_EXTERNAL_STORAGE
@@ -87,18 +76,13 @@ class MainActivity : ComponentActivity() {
                         }
                     )
                 }
-            }
         }
     }
 
     private fun startService() {
         if (!isServiceRunning) {
-            val intent = Intent(this, JetAudioService::class.java)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                startForegroundService(intent)
-            } else {
-                startService(intent)
-            }
+            val intent = Intent(this, PlayerService::class.java)
+            startForegroundService(intent)
             isServiceRunning = true
         }
     }
