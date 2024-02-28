82e957ab64ac9c8d451bd19a9679815a861da91f
Jeremy Leyvraz <jeremy.leyvraz@kep-technologies.com>
2024-02-28 08:32:06 +0100

    migration in progress

--- app/src/main/java/com/lj/app/MainActivity.kt
+++ app/src/main/java/com/lj/app/MainActivity.kt
@@ -3,6 +3,7 @@ package com.lj.app
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.PendingIntent
+import android.content.ComponentName
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.compose.material3.*
@@ -10,18 +11,27 @@ import android.os.Bundle
 import android.widget.RemoteViews
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.common.util.concurrent.MoreExecutors
 import com.lj.app.navigation.MainNavigation
 import com.lj.app.service.PlayerService
+import com.lj.app.viewmodel.PlayerViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -31,56 +41,65 @@ import dagger.hilt.android.AndroidEntryPoint
  */
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @SuppressLint("RemoteViewLayout")
-    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+    private val viewModel: PlayerViewModel by viewModels()
+    private var isServiceRunning = false
+
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Create a notification for the foreground service
-        val notificationId = 1
-        val notificationChannelId = "foreground_service_channel"
-        val notificationIntent = Intent(this, PlayerService::class.java)
-        val pendingIntent = PendingIntent.getActivity(
-            this,
-            0,
-            notificationIntent,
-            PendingIntent.FLAG_IMMUTABLE
-        )
-
-
-        // Create the notification layout using RemoteViews
-        val remoteViews = RemoteViews(this.packageName, R.layout.player)
-
-        val notification = NotificationCompat.Builder(this, notificationChannelId)
-            .setContentTitle("My Deezer example")
-            .setContentText("Playback service is running...")
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentIntent(pendingIntent)
-            .setCustomContentView(remoteViews)
-            .build()
-
-        val notificationManager = NotificationManagerCompat.from(this)
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.POST_NOTIFICATIONS
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.FOREGROUND_SERVICE),
-                123
-            )
-        }
-        notificationManager.notify(notificationId, notification)
-
-        // Start the player service
-        val serviceIntent = Intent(this, PlayerService::class.java)
-        startService(serviceIntent)
-
-        // Set content
         setContent {
-            val windowSizeClass = calculateWindowSizeClass(this)
-            MyApp(windowSizeClass = windowSizeClass)
+            JetAudioTheme {
+                // A surface container using the 'background' color from the theme
+                val permissionState = rememberPermissionState(
+                    permission = Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                val lifecycleOwner = LocalLifecycleOwner.current
+                DisposableEffect(key1 = lifecycleOwner) {
+                    val observer = LifecycleEventObserver { _, event ->
+                        if (event == Lifecycle.Event.ON_RESUME) {
+                            permissionState.launchPermissionRequest()
+                        }
+                    }
+                    lifecycleOwner.lifecycle.addObserver(observer)
+                    onDispose {
+                        lifecycleOwner.lifecycle.removeObserver(observer)
+                    }
+                }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen(
+                        progress = viewModel.progress,
+                        onProgress = { viewModel.onUiEvents(UIEvents.SeekTo(it)) },
+                        isAudioPlaying = viewModel.isPlaying,
+                        audiList = viewModel.audioList,
+                        currentPlayingAudio = viewModel.currentSelectedAudio,
+                        onStart = {
+                            viewModel.onUiEvents(UIEvents.PlayPause)
+                        },
+                        onItemClick = {
+                            viewModel.onUiEvents(UIEvents.SelectedAudioChange(it))
+                            startService()
+                        },
+                        onNext = {
+                            viewModel.onUiEvents(UIEvents.SeekToNext)
+                        }
+                    )
+                }
+            }
+        }
+    }
+
+    private fun startService() {
+        if (!isServiceRunning) {
+            val intent = Intent(this, JetAudioService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(intent)
+            } else {
+                startService(intent)
+            }
+            isServiceRunning = true
         }
     }
 }
---
+++ app/src/main/java/com/lj/app/model/Audio.kt
@@ -0,0 +1,13 @@
+package com.lj.app.model
+
+import android.net.Uri
+
+data class Audio(
+    val uri: Uri,
+    val displayName: String,
+    val id: Long,
+    val artist: String,
+    val data: String,
+    val duration: Int,
+    val title: String,
+)
